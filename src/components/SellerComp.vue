33fe6bc6d1af520a9e994838c2d2d3b7d4c17ce4
wangaijing <wangaijing@integritytech.com.cn>
2024-07-04 10:52:12 +0800

    商家销售统计图表，设置浏览器分辨率监听，拆分option

--- src/components/SellerComp.vue
+++ src/components/SellerComp.vue
@@ -21,13 +21,75 @@ export default {
   mounted() {
     this.initecharts()
     this.getData()
+    // 分辨率发生改变时，改变与窗口大小相关的参数
+    window.addEventListener('resize', this.screenUpdate)
+    this.screenUpdate()
   },
   destroyed() {
     clearInterval(this.timeId)
+    // 组件销毁后将监听器取消掉
+    window.removeEventListener('resize', this.screenUpdate)
   },
   methods: {
     initecharts() {
       this.chartIn = this.$echarts.init(this.$refs.seller_ref, 'chalk')
+      const initoptions = {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 1,
+          y2: 0,
+          colorStops: [{
+            offset: 0, color: '#5052EE' // 0% 处的颜色
+          }, {
+            offset: 1, color: '#AB6EE5' // 100% 处的颜色
+          }],
+          global: false // 缺省为 false
+        },
+        title: {
+          text: '︱商家销售统计',
+          left: 20,
+          top: 20,
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true,
+        },
+        xAxis: {
+          type: 'value',
+        },
+        yAxis: {
+          type: 'category',
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            z: 0,
+            lineStyle: {
+              color: '#2d3443',
+              // 解决背景分段问题
+              type: 'solid',
+            }
+          }
+        },
+        series: {
+          type: 'bar',
+          label: {
+            show: true,
+            position: 'right',
+            textStyle: {
+              color: 'white'
+            }
+          }
+        }
+      }
+      this.chartIn.setOption(initoptions)
+
       // 对图表进行鼠标的监听
       this.chartIn.on('mouseover', () => {
         clearInterval(this.timeId)
@@ -55,72 +117,15 @@ export default {
         return item.value
       })
 
-      const options = {
-        color: {
-          type: 'linear',
-          x: 0,
-          y: 0,
-          x2: 1,
-          y2: 0,
-          colorStops: [{
-            offset: 0, color: '#5052EE' // 0% 处的颜色
-          }, {
-            offset: 1, color: '#AB6EE5' // 100% 处的颜色
-          }],
-          global: false // 缺省为 false
-        },
-        title: {
-          text: '︱商家销售统计',
-          textStyle: {
-            fontSize: '60px'
-          },
-          left: 20,
-          top: 20,
-        },
-        grid: {
-          top: '20%',
-          left: '3%',
-          right: '6%',
-          bottom: '3%',
-          containLabel: true,
-        },
-        itemStyle: {
-          borderRadius: [0, 33, 33, 0],
-        },
-        xAxis: {
-          type: 'value',
-        },
+      const dataoptions = {
         yAxis: {
-          type: 'category',
           data: seleItem,
         },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'line',
-            z: 0,
-            lineStyle: {
-              width: 66,
-              color: '#2d3443',
-              // 解决背景分段问题
-              type: 'solid',
-            }
-          }
-        },
         series: {
-          type: 'bar',
           data: seleValue,
-          barWidth: 66,
-          label: {
-            show: true,
-            position: 'right',
-            textStyle: {
-              color: 'white'
-            }
-          }
         }
       }
-      this.chartIn.setOption(options)
+      this.chartIn.setOption(dataoptions)
     },
     startInterVal() {
       if (this.timeId) {
@@ -134,6 +139,33 @@ export default {
         }
         this.updateecharts()
       }, 3000)
+    },
+    // 当浏览器的分辨力发生改变时，调用方法完成适配
+    screenUpdate() {
+      const textSize = this.$refs.seller_ref.offsetWidth / 100 * 3.6
+      const adpterOptions = {
+        title: {
+          textStyle: {
+            fontSize: textSize
+          },
+        },
+        itemStyle: {
+          borderRadius: [0, textSize / 2, textSize / 2, 0],
+        },
+        tooltip: {
+          axisPointer: {
+            lineStyle: {
+              width: textSize,
+            }
+          }
+        },
+        series: {
+          barWidth: textSize,
+        }
+      }
+      this.chartIn.setOption(adpterOptions)
+      // 手动调用图表的resize
+      this.chartIn.resize()
     }
   }
 }
