8896e996a2e7dd123697a2de31919b152eed0762
wangaijing <wangaijing@integritytech.com.cn>
2024-07-24 13:43:57 +0800

    安装ele-ui

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import * as echarts from 'echarts'
 import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.less'
 import chalkTheme from '../public/static/theme/chalk.json'
 import vintageTheme from '../public/static/theme/vintage.json'
+
+Vue.use(ElementUI)
 
 // 请求基准路径的配置（请求的前缀）
 axios.defaults.baseURL = 'http://localhost:8888/api/'
 // 将axios、echarts挂在到vue的实例上，通过this.$http 和 this.$echarts使用
 Vue.prototype.$http = axios
 Vue.prototype.$echarts = echarts
-
 echarts.registerTheme('vintage', vintageTheme)
 echarts.registerTheme('chalk', chalkTheme)
 
